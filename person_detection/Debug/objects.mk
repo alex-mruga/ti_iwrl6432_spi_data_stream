3ed50c53cecbbd5e13b6465f700c5373fb94031e
95lux <jlx@posteo.net>
2025-01-03 17:59:36 +0100

    remove Debug folder

--- person_detection/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,-lfreertos.xwrL64xx.m4f.ti-arm-clang.debug.lib -Wl,-ldatapath_tracker2d.xwrL64xx.m4f.ti-arm-clang.debug.lib -Wl,-ldrivers.xwrL64xx.m4f.ti-arm-clang.debug.lib -Wl,-lboard.xwrL64xx.m4f.ti-arm-clang.debug.lib -Wl,-lcontrol.xwrL64xx.m4f.ti-arm-clang.debug.lib -Wl,-lutils.xwrL64xx.m4f.ti-arm-clang.debug.lib -Wl,-llibc.a -Wl,-lmmwavelink_m4.lib -Wl,-lfecss_rom_m4.lib -Wl,-lfecss_patch_m4.lib
-
--- person_detection/Debug/src/factory_cal.d
+++
@@ -1,3 +0,0 @@
-# FIXED
-
-src/factory_cal.o: ../src/factory_cal.c
