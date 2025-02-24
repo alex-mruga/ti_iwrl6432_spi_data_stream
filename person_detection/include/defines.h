b22a010024179956e618547acac6590517120130
95lux <jonas.bengt.lux@gmail.com>
2025-02-24 17:30:05 +0100

    remove old project

--- person_detection/include/defines.h
+++
@@ -1,7 +0,0 @@
-#define NUM_TX_ANTENNAS 2
-#define NUM_RX_ANTENNAS 3
-#define NUM_VIRT_ANTENNAS (NUM_TX_ANTENNAS*NUM_RX_ANTENNAS)
-#define NUM_ADC_SAMPLES 128 // number of adc samples per chirp hardcoded in https://dev.ti.com/gallery/view/mmwave/mmWaveSensingEstimator/ver/2.4.0/ and also used in MOTION_AND_PRESENCE_DETECTION_DEMO
-#define NUM_BURSTS_PER_FRAME 1 // from MOTION_AND_PRESENCE_DETECTION_DEMO
-#define NUM_CHIRPS_PER_BURST 8 // from MOTION_AND_PRESENCE_DETECTION_DEMO
-#define NUM_CHIRPS_PER_FRAME (NUM_BURSTS_PER_FRAME*NUM_CHIRPS_PER_BURST)
